66f10b18da522b68f7e11eff7e256b9a51bcddd0
kanayofirms <kanyeze@gmail.com>
2023-05-24 18:22:14 +0100

    Create functions for app functionality

---
+++ index.js
@@ -0,0 +1,28 @@
+let x = document.getElementById("login")
+let y = document.getElementById("register")
+let z = document.getElementById("btn")
+
+function register(){
+    x.style.left ="-400px"
+    y.style.left ="50px"
+    z.style.left ="110px"
+}
+
+function login(){
+    x.style.left="50px"
+    y.style.left="450px"
+    z.style.left="0"
+} 
+/*
+let submitEl = document.getElementById("submit-el")
+console.log(submitEl)
+
+let p = document.getElementById("error")
+console.log(p)
+
+function some(){
+    let all = "fill out the field";
+    p.textContent += all;
+    console.log(all)
+}
+*/
